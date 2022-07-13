41cc79c33a0e6a327d208f6b8686514835ed0383
jgaswer <98291918+jgaswer@users.noreply.github.com>
2022-07-13 18:42:12 +0700

    Update time.sh

--- time.sh
+++ time.sh
@@ -5,7 +5,7 @@ endtime=$(date -ud "$runtime" +%s)
 
 while [[ $(date -u +%s) -le $endtime ]]
 do
-    echo "Time Now: `date +%H:%M:%S`"
+    echo "jam crot: `date +%H:%M:%S`"
     echo "Sleeping for 1 minute"
     sleep 1m
 done
